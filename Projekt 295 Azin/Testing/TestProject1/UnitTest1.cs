c8294eadd96c43383d83ece7ea4632fd138729a0
azinIldas <azin.ildas@student.ipso.ch>
2023-11-13 22:43:25 +0100

    Änderung 1.8
    Unittesting machen example

--- Projekt 295 Azin/Testing/TestProject1/UnitTest1.cs
+++ Projekt 295 Azin/Testing/TestProject1/UnitTest1.cs
@@ -1,16 +1,37 @@
-namespace TestProject1
+using Microsoft.AspNetCore.Routing;
+using NUnit.Framework;
+using Projekt_295_Azin.Models;
+using Projekt_295_Azin.Models; // Namespace für Ihre Modelle
+using System;
+
+namespace Projekt_295_Azin.Tests
 {
-    public class Tests
+    [TestFixture]
+    public class BestellungenTests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void BestellungKannErstelltWerdenMitInitialWerten()
         {
+            // Arrangieren & Handeln
+            var bestellung = new Bestellungen
+            {
+                BestellungsId = 1,
+                Name = "Max Mustermann",
+                Emailadresse = "max@example.com",
+                Telefonnummer = "0123456789",
+                Lieferdatum = DateTime.Now,
+                Service = "Standard"
+            };
+
+            // Bestätigen
+            Assert.AreEqual(1, bestellung.BestellungsId);
+            Assert.AreEqual("Max Mustermann", bestellung.Name);
+            Assert.AreEqual("max@example.com", bestellung.Emailadresse);
+            Assert.AreEqual("0123456789", bestellung.Telefonnummer);
+            Assert.That(bestellung.Lieferdatum, Is.EqualTo(DateTime.Now).Within(TimeSpan.FromSeconds(5)));
+            Assert.AreEqual("Standard", bestellung.Service);
         }
 
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
+
     }
 }
