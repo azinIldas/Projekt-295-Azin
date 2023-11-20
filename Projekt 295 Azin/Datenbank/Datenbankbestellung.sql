beb79da5d05686342be7286b99ebe1a05dd0a58b
azinIldas <azin.ildas@student.ipso.ch>
2023-11-20 10:02:31 +0100

    Änderung 1.23
    JWT Tabelle hinzufügen

--- Projekt 295 Azin/Datenbank/Datenbankbestellung.sql
+++ Projekt 295 Azin/Datenbank/Datenbankbestellung.sql
@@ -30,3 +30,31 @@ CREATE TABLE Bestellungen (
     -- Optionale Spalte für zusätzliche Serviceinformationen
     Service VARCHAR(50)
 );
+
+-- Um inhalt zu checken
+Select * From Bestellungen;
+
+---------------------------------
+--- Logging Tabelle erstellen ---
+USE BackendSkiService;
+
+-- Erstellt eine neue Tabelle 'Benutzer'
+CREATE TABLE Benutzer (
+    -- Primärschlüssel der Tabelle, automatisch inkrementierend beginnend bei 1
+    BenutzerID INT IDENTITY(1, 1) PRIMARY KEY,
+
+    -- Spalte für den Benutzernamen, darf nicht NULL sein
+    Name VARCHAR(255) NOT NULL,
+
+    -- Spalte für das Passwort, darf nicht NULL sein
+    Password VARCHAR(255) NOT NULL,
+
+    -- Spalte für den Adminstatus,  true oder false
+    AdminStatus BIT NOT NULL
+);
+
+-- Optional: Einfügen eines Testbenutzers in die Tabelle
+INSERT INTO Benutzer (Name, Password, AdminStatus) VALUES ('TestUser', 'TestPasswort', 0);
+
+-- Um den Inhalt der Tabelle zu überprüfen
+SELECT * FROM Benutzer;
